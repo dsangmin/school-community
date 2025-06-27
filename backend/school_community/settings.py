043f0eb433f791f72e1d1e0bf44baa304701ea3b
dsangmin <74703334+dsangmin@users.noreply.github.com>
2025-06-28 00:33:03 +0900

    add main

--- backend/school_community/settings.py
+++ backend/school_community/settings.py
@@ -1,2 +1,12 @@
 ALLOWED_HOSTS = ['dsangmin2336.pythonanywhere.com']
 ROOT_URLCONF = 'school_community.urls'
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'django.contrib.users',
+    'django.contrib.posts',
+]
