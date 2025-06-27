aebf1a8c25bc173f85857851c1bd187801b96166
dsangmin <74703334+dsangmin@users.noreply.github.com>
2025-06-28 00:39:03 +0900

    add main

--- backend/school_community/urls.py
+++ backend/school_community/urls.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import 
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("Hello, this is the School Community homepage!")
 
 urlpatterns = [
+    path('', home),
     path('admin/', admin.site.urls),
 ]
