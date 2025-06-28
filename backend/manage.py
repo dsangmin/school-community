f640a066bca53c02813bd9f6346b98e66da9976d
dsangmin <74703334+dsangmin@users.noreply.github.com>
2025-06-28 11:44:20 +0900

    add main

--- backend/manage.py
+++ backend/manage.py
@@ -2,7 +2,7 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "school_community.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "board.school_community.settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
